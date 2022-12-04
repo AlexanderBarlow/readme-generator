eeae9ee92253e1f1acba6caa8133fce564c91ff7
Alexander Barlow <alex.barlow@rocketmail.com>
2022-12-04 14:33:48 -0500

    initialized the prompt through the init function and the wrote the file throught the writeToFile function

--- index.js
+++ index.js
@@ -30,22 +30,22 @@ const questions = () => {
         type: 'list',
         message: 'What kind of license would you like your project to have?',
         name: 'license'
-    }
+    },
     {
         type: 'input',
         message: 'What command should be run to install dependencies?',
         name: 'dependencies'
-    }
+    },
     {
         type: 'input',
         message: 'What command should be run to run tests?',
         name: 'tests'
-    }
+    },
     {
         type: 'input',
         message: 'What does the user need to know about using the repo?',
         name: 'repo'
-    }
+    },
     {
         type: 'input',
         message: 'What does the user need to know about contributing to the repo?',
@@ -57,10 +57,22 @@ const questions = () => {
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data => {
+        console.log('Your Readme file has been generated.')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions()
+    .then(input => {
+        return generate(input);
+    })
+    .then((error, markdown) => {
+        writeToFile('./dist/README.md', markdown);
+    })
+}
 
 // Function call to initialize app
 init();
