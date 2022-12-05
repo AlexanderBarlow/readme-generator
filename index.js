b375628fda7dc7cf56a3fa92ef911fa3e55ab09f
Alexander Barlow <alex.barlow@rocketmail.com>
2022-12-05 15:16:29 -0500

    removed functions in index.js for then statements

--- index.js
+++ index.js
@@ -52,28 +52,21 @@ const questions = () => {
         message: 'What does the user need to know about contributing to the repo?',
         name: 'contribute'
     }])
-    .then(input => {
+    .then (input => {
         return input;
     })
-}
-
-// TODO: Create a function to write README file
-const writeToFile = (input) => {
-    fs.writeFile(`${input.Title}.md`, input, (err) => 
-        err? console.error(err) : console.log('Your Readme file has been generated.')
-    );
-};
-
-// TODO: Create a function to initialize app
-const init = () => {
-    questions()
     .then(input => {
         return generate(input);
     })
     .then(input => {
-        writeToFile(input);
+        console.log(input.Title)
+        fs.writeFile(`${input.Title}.md`, (input), (err) => {
+        err? console.error(err) : console.log('Your Readme file has been generated.')
+        });
     })
 }
 
+// TODO: Create a function to initialize app
+const init = () => questions()
 // Function call to initialize app
 init();
